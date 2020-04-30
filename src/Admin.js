4df52bb2efa2ae00f231c52bbdde6977af3cc3f3
xiechangwang <1113868253@qq.com>
2020-04-30 17:12:30 +0800

    配置less，less-loader，antd按需加载。。。

---
+++ src/Admin.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Button} from 'antd';
+
+class Admin extends Component{
+  render(){
+    return (
+      <div>
+        <Button>antd</Button>
+      </div>
+    )
+  }
+}
+
+export default Admin;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Button from './Admin';
 
 function App() {
   return (
     <div className="App">
-      123132213
+      <Button/>
     </div>
   );
 }
