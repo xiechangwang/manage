0447abd86fea2198faa5d4276acc90d60c3e851e
xiechangwang <1113868253@qq.com>
2020-05-07 16:34:26 +0800

    代码提交基本布局。。。。

--- src/Admin.js
+++ src/Admin.js
@@ -1,11 +1,26 @@
 import React,{Component} from 'react';
-import {Button} from 'antd';
+import {Row,Col} from 'antd';
+import './style/common.less';
+import NavLeft from './components/NavLeft';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class Admin extends Component{
   render(){
     return (
       <div>
-        <Button>antd</Button>
+        <Row className="container">
+          <Col span={3} className="nav-left" >
+            <NavLeft/>
+          </Col>
+          <Col span={21} className="main" >
+            <Header/>
+            <Row className="content">
+              cont
+            </Row>
+            <Footer/>
+          </Col>
+        </Row>
       </div>
     )
   }
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import MenuConfig from '../../config/menuConfig';
+import './index.less';
+import { Menu } from 'antd';
+
+const { SubMenu } = Menu;
+
+class NavList extends Component {
+  componentWillMount(){
+    const menuTreeNode=this.renderMenu(MenuConfig);
+    this.setState({
+      menuTreeNode
+    })
+  }
+  //菜单渲染
+  renderMenu=(data)=>{
+    return data.map((item)=>{
+      if(item.children){
+        this.renderMenu(item.children);
+        return(
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return(
+      <Menu.Item key={item.key}>{item.title}</Menu.Item>
+      )
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div className="logo">
+          <img src="/assets/logo.svg" alt="" />
+          <h1>Imooc MS</h1>
+        </div>
+        <Menu theme={'dark'} >
+          {/* <SubMenu key="sub1" title="Navigation One">
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub4"  title="Navigation Three">
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <Menu.Item key="11">Option 11</Menu.Item>
+            <Menu.Item key="12">Option 12</Menu.Item>
+          </SubMenu> */}
+          {this.state.menuTreeNode}
+        </Menu>
+      </div>
+    )
+  }
+};
+
+export default NavList;
